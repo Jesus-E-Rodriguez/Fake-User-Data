ac394977c8be53dee2dbe69466eb908d0a3e8c16
Jesus Rodriguez <jrodri69@email.cpcc.edu>
2018-07-31 22:50:53 -0400

    First Commit

---
+++ author.js
@@ -0,0 +1,92 @@
+[
+     {
+          "authorID" : 1,
+          "authorName" : "Jaylen Kim",
+          "authorImage": "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+          "authorDescription" : "Curabitur auctor elit sit amet arcu vulputate, eu fringilla erat molestie. Nam a arcu a leo vehicula tempor.",
+          "AuthorLocation" : "New York",
+          "authorDesigns" : 150,
+          "authorFollowers" : 1200
+     },
+     {
+          "authorID" : 2,
+          "authorName" : "Jonathan Smith",
+          "authorImage": "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+          "authorDescription" : "Ellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
+          "AuthorLocation" : "San Francisco, CA",
+          "authorDesigns" : 959,
+          "authorFollowers" : 3101
+     },
+     {
+          "authorID" : 3,
+          "authorName" : "Mike Sanders",
+          "authorImage": "https://images.pexels.com/photos/1271132/pexels-photo-1271132.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+          "authorDescription" : "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
+          "AuthorLocation" : "Boulder, CO",
+          "authorDesigns" : 15,
+          "authorFollowers" : 10
+     },
+     {
+          "authorID" : 4,
+          "authorName" : "Tom Henry",
+          "authorImage": "https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+          "authorDescription" : "Aliquam accumsan magna quis odio fermentum auctor. Mauris porttitor dui eget lectus fringilla sollicitudin.",
+          "AuthorLocation" : "DC",
+          "authorDesigns" : 132,
+          "authorFollowers" : 104
+     },
+     {
+          "authorID" : 5,
+          "authorName" : "Lana Del Mar",
+          "authorImage": "https://images.pexels.com/photos/875862/pexels-photo-875862.png?auto=compress&cs=tinysrgb&h=750&w=1260",
+          "authorDescription" : "Etiam elementum elit urna, nec sagittis quam lobortis non...",
+          "AuthorLocation" : "Mexico",
+          "authorDesigns" : 2791,
+          "authorFollowers" : 22000
+     },
+     {
+          "authorID" : 6,
+          "authorName" : "Selena Rummage",
+          "authorImage": "https://images.pexels.com/photos/372042/pexels-photo-372042.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+          "authorDescription" : "Donec ac ullamcorper velit. Cras laoreet orci neque, malesuada tincidunt libero condimentum quis.",
+          "AuthorLocation" : "Tennessee",
+          "authorDesigns" : 2791,
+          "authorFollowers" : 22000
+     },
+     {
+          "authorID" : 7,
+          "authorName" : "Jason Wallbord",
+          "authorImage": "https://images.pexels.com/photos/212286/pexels-photo-212286.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+          "authorDescription" : "Donec ac ullamcorper velit. Cras laoreet orci neque, malesuada tincidunt libero condimentum quis.",
+          "AuthorLocation" : "Hampshire, MA",
+          "authorDesigns" : 5,
+          "authorFollowers" : 1
+     },
+     {
+          "authorID" : 8,
+          "authorName" : "Bernie Mckingsley",
+          "authorImage": "https://images.pexels.com/photos/1254690/pexels-photo-1254690.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+          "authorDescription" : "Duis at sapien egestas, porttitor justo eget, mattis ligula!!!",
+          "AuthorLocation" : "Kentucky",
+          "authorDesigns" : 67,
+          "authorFollowers" : 98
+     },
+     {
+          "authorID" : 9,
+          "authorName" : "Karl Watson",
+          "authorImage": "https://images.pexels.com/photos/91224/pexels-photo-91224.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+          "authorDescription" : "Vivamus dolor nibh, egestas vitae libero non, lobortis laoreet risus.",
+          "AuthorLocation" : "Des Moines, IA",
+          "authorDesigns" : 1,
+          "authorFollowers" : 0
+     },
+     {
+          "authorID" : 10,
+          "authorName" : "Sakura Kurosaki",
+          "authorImage": "https://images.pexels.com/photos/253758/pexels-photo-253758.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+          "authorDescription" : "Fusce fringilla tincidunt purus id tempor.",
+          "AuthorLocation" : "Japan",
+          "authorDesigns" : 451,
+          "authorFollowers" : 9501
+     }
+]
---
+++ content.js
@@ -0,0 +1,102 @@
+[
+    {
+          "authorID": 1, 
+          "contentID" : 1,
+          "contentImage": "https: //images.pexels.com/photos/68672/beach-beverage-caribbean-cocktail-68672.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+          "contentTitle": "Beach Drinks",
+          "contentBody": "Lorem ipsum dolor, sit amet consectetur adipisicing elit.Soluta, quos nulla impedit odio nobis vitae perspiciatis rem magni rerum! Quos nobis maxime nihil adipisci esse non blanditiis sed ipsum, autem laudantium expedita ratione velit facere voluptatibus laborum, eum sit optio fugit ? Rem dolor numquam eos a suscipit natus illum quo.",
+          "views": 300,
+          "comments": 22,
+          "favourited": 35
+    },
+    {
+          "authorID": 2, 
+          "contentID" : 1,
+          "contentImage": "https://images.pexels.com/photos/374780/pexels-photo-374780.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+          "contentTitle": "Coffee",
+          "contentBody": "Aenean mauris tortor, tincidunt in congue a, iaculis venenatis massa. Vestibulum sed lobortis quam. Duis vel luctus elit. Sed purus odio, pellentesque nec urna eu, facilisis porta orci.",
+          "views": 2000,
+          "comments": 151,
+          "favourited": 98
+    },
+    {
+          "authorID": 3, 
+          "contentID" : 1,
+          "contentImage": "https://images.pexels.com/photos/1279330/pexels-photo-1279330.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+          "contentTitle": "Lo Mein",
+          "contentBody": "Donec euismod, enim a bibendum placerat, arcu lorem dignissim lacus, vel semper nisl velit eget mauris.",
+          "views": 100,
+          "comments": 11,
+          "favourited": 5
+    },
+    {
+          "authorID": 4, 
+          "contentID" : 1,
+          "contentImage": "https://images.pexels.com/photos/991012/pexels-photo-991012.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+          "contentTitle": "My Summer Vacation",
+          "contentBody": "Vestibulum sed lobortis quam. Duis vel luctus elit. Sed purus odio, pellentesque nec urna eu, facilisis porta orci.",
+          "views": 532,
+          "comments": 101,
+          "favourited": 44
+    },
+    {
+          "authorID": 5, 
+          "contentID" : 1,
+          "contentImage": "https://images.pexels.com/photos/1048024/pexels-photo-1048024.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+          "contentTitle": "Minimalist Cactus",
+          "contentBody": "Ut mollis arcu non nunc luctus cursus. Donec vitae turpis eleifend, lobortis augue nec, condimentum ante.",
+          "views": 10000,
+          "comments": 989,
+          "favourited": 732
+    },
+    {
+          "authorID": 6, 
+          "contentID" : 1,
+          "contentImage": "https://images.pexels.com/photos/529927/pexels-photo-529927.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+          "contentTitle": "Setting up my workspace",
+          "contentBody": "Ut mollis arcu non nunc luctus cursus. Donec vitae turpis eleifend, lobortis augue nec, condimentum ante.",
+          "views": 10,
+          "comments": 0,
+          "favourited": 1
+    },
+    {
+          "authorID": 7, 
+          "contentID" : 1,
+          "contentImage": "https://images.pexels.com/photos/1095550/pexels-photo-1095550.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+          "contentTitle": "My Avacado Toast",
+          "contentBody": "Donec ligula ex, sodales fringilla gravida sit amet, pretium eu nisl. Maecenas in diam orci.",
+          "views": 100,
+          "comments": 15,
+          "favourited": 7
+    },
+    {
+          "authorID": 8, 
+          "contentID" : 1,
+          "contentImage": "https://images.pexels.com/photos/1092730/pexels-photo-1092730.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+          "contentTitle": "My Breakfast Routine",
+          "contentBody": "Vivamus a ligula mi. Pellentesque auctor sit amet urna sed vestibulum.",
+          "views": 43,
+          "comments": 21,
+          "favourited": 30
+    },
+    {
+          "authorID": 9, 
+          "contentID" : 1,
+          "contentImage": "https://images.pexels.com/photos/793166/pexels-photo-793166.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+          "contentTitle": "Vacation Location",
+          "contentBody": "Maecenas iaculis elit a diam lacinia, et pharetra ipsum placerat.",
+          "views": 3,
+          "comments": 0,
+          "favourited": 0
+    },
+    {
+          "authorID": 10, 
+          "contentID" : 1,
+          "contentImage": "https://images.pexels.com/photos/934718/pexels-photo-934718.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+          "contentTitle": "Enjoying the mountainside",
+          "contentBody": "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
+          "views": 300,
+          "comments": 255,
+          "favourited": 105
+    }
+]
